ac29673270adef146105829bddf3a1bea88af1a7
Yasmin Ramirez <yasmin.ramirez.sanchez19@gmail.com>
2023-11-06 11:43:21 -0600

    Add Error component for errors when fetching http requests

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 
 const requestConfig = {};
 
@@ -25,10 +26,13 @@ const Meals = () => {
     error,
   } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-  console.log(loadedMeals);
-
   if (isLoading) {
-    return <p>Fetching meals...</p>;
+    return <p className="center">Fetching meals...</p>;
+  }
+
+  if (error) {
+    console.log("Error");
+    return <Error title="Failed to fetch meals" message={error} />;
   }
 
   return (
